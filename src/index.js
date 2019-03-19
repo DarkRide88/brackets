ca3fc13c28d997332706e086dc2394b72b88bc61
DarkRide88 <dark88ride@mail.ru>
2019-03-19 15:02:11 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let inpStr = str.split('');   
+    let openBracket;
+    let closeBracket;  
+        for(let i = 0; i < inpStr.length; i++){
+            for(let j = 0; j < bracketsConfig.length; j++){
+                openBracket = bracketsConfig[j][0] 
+                closeBracket = bracketsConfig[j][1]              
+                if(inpStr[i] == openBracket && inpStr[i+1] == closeBracket){
+                    inpStr.splice(i,2)
+                    i = -1;  
+                }  else {                   
+                    continue;
+                }               
+            } 
+        }  
+        if(inpStr.length == 0){
+            return true
+        } else {
+            return false
+        }    
+    }
